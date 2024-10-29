6d49778dd124997a7dc47fad8aba19baaf4b1c8f
Adam Johnson <87151710+Ajohn10@users.noreply.github.com>
2024-10-29 15:45:53 -0400

    Added System.Text.Json serializer for Rest client.  Added missing solution file from previous commit

---
+++ RESTClient.SystemTextJsonSerializer/SystemTextJsonSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace RESTClient.Serializers
+{
+	/// <summary>
+	/// serializer for RESTClient that uses the System.Text.Json dependency
+	/// </summary>
+	public class SystemTextJsonSerializer
+	{
+		public string MediaType { get; set; } = "application/json";
+
+		public System.Text.Encoding Encoding { get; set; } = System.Text.Encoding.Default;
+
+		public T Deserialize<T>(string body) => JsonSerializer.Deserialize<T>(body);
+
+		public string Serialize(object ob) => JsonSerializer.Serialize(ob);
+	}
+}
